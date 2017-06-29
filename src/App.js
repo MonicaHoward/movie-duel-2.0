9c09cdba7f4113404b5aa9c4f46cd017d4bcb15b
Monica Howard <jay.m.howard@gmail.com>
2017-06-29 18:39:49 -0500

    couldnt get React Router to work - will try again later

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Header from  './Header.js';
 import MovieDuel from './MovieDuel.js';
 import About from './About.js';
@@ -7,11 +8,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <MovieDuel />
-        <About />
-      </div>
+      // <Router>
+        <div className="App">
+          <Header />
+            {/* <Link to="/movie-duel"> */}
+              <MovieDuel />
+            {/* </Link>
+            <Link to="/about"> */}
+              <About />
+            {/* </Link>
+            <Route path="/movie-duel" component={ MovieDuel } />
+            <Route path="/movie-search" component={ MovieSearch } />
+            <Route path="/about"  component={ About } /> */}
+        </div>
+      // </Router>
+
     );
   }
 }
