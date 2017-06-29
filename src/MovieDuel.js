b8a751f8fb744f3bc0568cf0d9a5e3e6daa6dc33
Monica Howard <jay.m.howard@gmail.com>
2017-06-28 23:33:46 -0500

    cleaned up all files for a fresh start

--- src/MovieDuel.js
+++ src/MovieDuel.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import MovieSearch from './MovieSearch.js';
+
 
 class MovieDuel extends React.Component {
   render() {
     return (
       <div>
-        <MovieSearch
-          className="movie-search"/>
-        <MovieSearch
-          className="movie-search"/>
+  
       </div>
     )
   }
