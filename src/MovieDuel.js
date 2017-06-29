825e2002e5140a10e5b05d4fdb707ea5b54cb4d5
Monica Howard <jay.m.howard@gmail.com>
2017-06-29 02:44:54 -0500

    off to a great start

--- src/MovieDuel.js
+++ src/MovieDuel.js
@@ -1,11 +1,56 @@
 import React from 'react';
 
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>MOVIE DUEL</h1>
+        <ul className="nav">
+          <li>about</li>
+          <li>movie search</li>
+          <li>movie duel</li>
+        </ul>
+      </div>
+    )
+  }
+}
+class Input extends React.Component {
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="enter a movie title..."/>
+      </form>
+    )
+  }
+}
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <li className="movie-result">
+        <div className="movie-poster">
+          {/* img tags goes here */}
+        </div>
+        <div className="movie-info">
+          <h1 className="movie-title">Movie Title</h1>
+          <p>Synopsis</p>
+        </div>
+        <div className="movie-score">
+          <h2>SCORE:</h2>
+          <h1>X</h1>
+        </div>
+      </li>
+    )
+  }
+}
 
 class MovieDuel extends React.Component {
   render() {
     return (
       <div>
-  
+        <Header />
+        <Input />
+        <SearchResults />
       </div>
     )
   }
